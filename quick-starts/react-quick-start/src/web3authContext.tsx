b946011e93c724018c14ce53aecd10721a495422
Yashovardhan Agrawal <21066442+yashovardhan@users.noreply.github.com>
2025-10-02 15:18:24 +0800

    Test Sei Issue

--- quick-starts/react-quick-start/src/web3authContext.tsx
+++ quick-starts/react-quick-start/src/web3authContext.tsx
@@ -1,10 +1,11 @@
 // IMP START - Quick Start
+import "@sei-js/sei-global-wallet/eip6963"
 import { WEB3AUTH_NETWORK } from "@web3auth/modal";
 import { type Web3AuthContextConfig } from "@web3auth/modal/react";
 // IMP END - Quick Start
 
 // IMP START - Dashboard Registration
-const clientId = "BHgArYmWwSeq21czpcarYh0EVq2WWOzflX-NTK-tY1-1pauPzHKRRLgpABkmYiIV_og9jAvoIxQ8L3Smrwe04Lw"; // get from https://dashboard.web3auth.io
+const clientId = "BCqaSvZY2H0pd9asnWeOlI9J2j9WrVorFcBZwnMnnLb-ms8CBnJXD5nvZGMpzQxofkpHE0zYZLI7IQ_0dr3Vt9Q"; // get from https://dashboard.web3auth.io
 // IMP END - Dashboard Registration
 
 // IMP START - Config
