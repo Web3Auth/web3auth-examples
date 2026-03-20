703a63ebcfc5902984b858964fd7b7f889a93ec8
Yashovardhan Agrawal <21066442+yashovardhan@users.noreply.github.com>
2026-03-20 16:05:45 +0530

    Update environment.prod.ts

--- quick-starts/angular-quick-start/src/environments/environment.prod.ts
+++ quick-starts/angular-quick-start/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // Replace with your production Client ID from https://dashboard.web3auth.io
   // Use Sapphire Mainnet for production — it does NOT allow localhost
-  clientId: "",
+  clientId: "BHgArYmWwSeq21czpcarYh0EVq2WWOzflX-NTK-tY1-1pauPzHKRRLgpABkmYiIV_og9jAvoIxQ8L3Smrwe04Lw",
 };
